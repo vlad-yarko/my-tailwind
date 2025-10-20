c37d745d77ce53e81aa3bc824dc59769ec99ed0a
vlad-yarko <vladyarik88888@gmail.com>
2025-10-20 20:34:01 +0300

    studying

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			colors: {
+				myColor: '#ffbb00', // your custom color
+			},
+		},
+	},
+	plugins: [
+		
+	],
+}
